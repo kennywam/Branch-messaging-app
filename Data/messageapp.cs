8d9cea35365457d079b0fad7524781e912fc238d
Kennedy Wambua <kennywambu254@gmail.com>
2023-12-04 00:12:01 +0300

    adding database schema classes

--- Data/messageapp.cs
+++ Data/messageapp.cs
@@ -1,6 +1,22 @@
-﻿namespace Branch_app.Data
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Branch_app.Data
 {
+    [BsonIgnoreExtraElements]
     public class messageapp
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string messageId { get; set; }
+
+        [BsonElement("User ID")]
+        public string userId { get; set; }
+
+        [BsonElement("Timestamp (UTC)")]
+        public string timeStamp { get; set; }
+
+        [BsonElement("Message Body")]
+        public string message { get; set; }
     }
 }
