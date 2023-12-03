b2494ac11ec6f53cf286512a3deebd7841f0c46c
Kennedy Wambua <kennywambu254@gmail.com>
2023-12-04 00:12:22 +0300

    database settings

--- Data/messageappDbSettings.cs
+++ Data/messageappDbSettings.cs
@@ -2,5 +2,8 @@
 {
     public class messageappDbSettings
     {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string BranchAppCollectionName { get; set; } = string.Empty;
     }
 }
